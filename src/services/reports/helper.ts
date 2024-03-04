c0ccdd0442b5e12baf11ca7f2ee91b2a66c54d86
Alejandro Jorgen Marten <ajorgenmarten35@gmail.com>
2024-03-04 14:56:44 -0500

    vinculo con tabla users

--- src/services/reports/helper.ts
+++ src/services/reports/helper.ts
@@ -2,17 +2,17 @@ import { ObjectId } from "mongoose"
 import { ReportModel } from "./models"
 import lang from "../../lang";
 
-export const getMyReports = async ( userId: ObjectId, page: number = 1 ) => {
+export const getMyReports = async ( userId: ObjectId, page: number = 0 ) => { 
     if (page < 0) throw new Error(lang.services.reports.helper.getMyReportPageError);
     
-    const itemsPerPage = 2
+    const itemsPerPage = 50
     const skip = page * itemsPerPage
 
     const totalRegisters = await ReportModel.find({ author: userId }).count()
     const totalPages = Math.ceil( totalRegisters / itemsPerPage)
-    const reports = await ReportModel.find({author: userId}).skip(skip).limit(itemsPerPage)
+    const reports = await ReportModel.find({author: userId}).skip(skip).limit(itemsPerPage).sort( { createdAt: "descending" } ).populate('author')
 
-    const prev = page ? page : undefined
+    const prev = page ? page - 1 : undefined
     const current = page
     const next = totalPages > (page + 1) ? (page + 1) : undefined
 
